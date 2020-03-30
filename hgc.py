cd15d22de959198e998c8204a52db36e392dd0cd
Shruti Patel <43066790+shrutipatel11@users.noreply.github.com>
2020-03-30 10:18:41 +0530

    Issue in global dictionary updation

--- hgc.py
+++ hgc.py
@@ -6,11 +6,11 @@ from multiprocessing import Process
 from subprocess import Popen, PIPE
 import socket
 
-tpc_results = {'src':''}
-tpc_executed = {'src':0}
+tpc_results = {}
+tpc_executed = {}
 processes = ['src']
-hgc_executed = {'src':0}        #0 if the task is not executed, 1 if it is executed
-hgc_results = {'src':''}     #stores the output of a task in the form of a string(argument)
+hgc_executed = {}     #0 if the task is not executed, 1 if it is executed
+hgc_results = {}     #stores the output of a task in the form of a string
 
 tasks = []
 edges = []
@@ -33,7 +33,7 @@ def get_result_from_3pc():
         if arr[0]=='sink':
             break
 
-#Find children of a node
+#Find children of a node(task)
 def find_children(edge_list, parent):
     children = []
     for element in edge_list:
@@ -41,7 +41,7 @@ def find_children(edge_list, parent):
             children.append(element[1])
     return children
 
-#Find parents of a node
+#Find parents of a node(task)
 def find_parents(edge_list, child):
     parents = []
     for element in edge_list:
@@ -49,73 +49,35 @@ def find_parents(edge_list, child):
             parents.append(element[0])
     return parents
 
+#Run the processes on different cores
 def run_process(process):
     # x = os.system('python {}.py'.format(process))
     stdout = Popen('python {}'.format(process), shell=True, stdout=PIPE).stdout
     output = ast.literal_eval(stdout.read().rstrip())
     return output
 
-# Read the task graph
-f = open("bench","r")
-if f.mode=="r":
-    contents = f.readlines()
-
-    #store the graph nodes(tasks) and dependencies(edges) in the lists
-    for x in contents:
-        if x.find("TASK")!=-1:
-            start_index = 5
-            while True:
-                if x[start_index]==' ':
-                    break
-                start_index+=1
-            task_name = x[5:start_index]
-            tasks.append(task_name)  #add the task to the task list
-
-        elif x.find("ARC")!=-1:
-            start_index = 14
-            while True:
-                if x[start_index]==' ':
-                    break
-                start_index+=1
-            t1 = x[14:start_index]          #first task of the edge
-
-            start_index2 = start_index+4
-            while True:
-                if x[start_index2]==' ':
-                    break
-                start_index2+=1
-            t2 = x[start_index+4:start_index2]      #second task of the edge
-
-            edges.append([t1,t2])    #add the edge to the edge list
-
-#Schedule the processes and start execution with the node 'src'
-# processes = ['src.py', 'dotproduct.py 1,2,3,4,5,6']
-# k=2
-# pool = Pool(processes=k)
-# z = pool.map(run_process, processes)
-# print type(z)
-
 
 def process_task_graph():
     while True:
         print processes
         print tpc_results
+
         #remove the processes whose ancestors have not yet completed the execution in 3pc and hgc both
         if len(processes)!=1 and processes[0]!='src':
             rmproc = []
             for i in processes:
                 p = find_parents(edges,i)
                 for j in p:
-                    if hgc_executed[j]!=1 and (j not in tpc_results):
-                        rmproc.append(i)
-                        break
+                    if hgc_executed[j]!=1 and (j not in tpc_results):       #ISSUE 1 : tpc_results is always empty empty so it removes the processses
+                        rmproc.append(i)                                    #for which we have the inpur fron 3PC. tpc_results is updated in the
+                        break                                               #function get_result_from_3pc
             for i in rmproc:
                 processes.remove(i)
                 print "removed",i
 
         # print "processes",processes
 
-        #for the remaining processes,find the inputs (arguments) from the outputs stored in hgc_results dict
+        #for the remaining processes,find the inputs (arguments) from the outputs stored in hgc_results or tpc_results dict
         cmdarg = []
         for j in processes:
             tempstr = j
@@ -127,7 +89,7 @@ def process_task_graph():
                     tempstr+=hgc_results[k]
                     tempstr+=' '
                 else:
-                    print "Output taken from 3PC"
+                    print "Output taken from 3PC"                           #ISSUE 2 : Control never goes to the else block due to ISSUE 1
                     tempstr+=tpc_results[k]
                     tempstr+=' '
             cmdarg.append(tempstr)
@@ -171,6 +133,44 @@ def process_task_graph():
             processes.remove(i)
 
         time.sleep(1)
+
+
+#--------------------------------------Main function------------------------------------------#
+
+# Read the task graph
+f = open("bench","r")
+if f.mode=="r":
+    contents = f.readlines()
+
+    #store the graph nodes(tasks) and dependencies(edges) in the lists
+    for x in contents:
+        if x.find("TASK")!=-1:
+            start_index = 5
+            while True:
+                if x[start_index]==' ':
+                    break
+                start_index+=1
+            task_name = x[5:start_index]
+            tasks.append(task_name)  #add the task to the task list
+
+        elif x.find("ARC")!=-1:
+            start_index = 14
+            while True:
+                if x[start_index]==' ':
+                    break
+                start_index+=1
+            t1 = x[14:start_index]          #first task of the edge
+
+            start_index2 = start_index+4
+            while True:
+                if x[start_index2]==' ':
+                    break
+                start_index2+=1
+            t2 = x[start_index+4:start_index2]      #second task of the edge
+
+            edges.append([t1,t2])    #add the edge to the edge list
+
+
 
 p1 = Process(target = get_result_from_3pc)
 p1.start()
