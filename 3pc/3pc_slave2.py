ddbf66531b9e8702c23e24b5fe6803f4db512b23
Shruti Patel <43066790+shrutipatel11@users.noreply.github.com>
2020-05-02 11:22:36 +0530

    3pc slaves

---
+++ 3pc/3pc_slave2.py
@@ -0,0 +1,31 @@
+import os
+import ast
+from multiprocessing import Pool, Process, Manager
+from subprocess import Popen, PIPE
+import socket
+import select
+
+s = socket.socket()
+port = 10002
+s.connect(('127.0.0.1', port))
+
+while True:
+    timeout = 0.5  # in seconds
+    ready_sockets, _, _ = select.select([s], [], [], timeout)
+    if ready_sockets:
+        cmdarg = s.recv(1024)
+        if len(cmdarg)>0:
+            print "--------------------------------------------------------------------"
+            print "Received",cmdarg
+            stdout = Popen('python {}'.format(cmdarg), shell=True, stdout=PIPE).stdout
+            output = ast.literal_eval(stdout.read().rstrip())
+            print "Output",output
+
+            tname = cmdarg.split(" ")[0][:-3]
+            outstr = ''
+            for i in range(len(output)):
+                outstr += str(output[i])
+                if i!=len(output)-1:
+                    outstr+=','
+            sendString = tname+' '+outstr
+            s.send(sendString)
